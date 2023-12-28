850d541aeac4a8b2a93da6954e2e6637f08e3d9f
Saul Paredes <saulkurosaki13@gmail.com>
2023-12-28 14:43:38 -0600

    install and setup clerk auth

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,10 +2,5 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main>
-      <h4 className="text-4xl">Evently</h4>
-      <Button>Hello</Button>
-    </main>
-  );
+  return <main></main>;
 }
