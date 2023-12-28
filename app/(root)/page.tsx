4c4b224453c9972c7ac2876bcd7221dc33057194
Saul Paredes <saulkurosaki13@gmail.com>
2023-12-28 10:01:00 -0600

    implement complete global layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
+      <h4 className="text-4xl">Evently</h4>
       <Button>Hello</Button>
     </main>
   );
