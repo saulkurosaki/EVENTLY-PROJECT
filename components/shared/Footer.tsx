03f8b721d96ac77120d094aa155236eeffdd625e
Saul Paredes <saulkurosaki13@gmail.com>
2023-12-28 14:18:13 -0600

    implement root layout initial structure

---
+++ components/shared/Footer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <div>Footer Test c:</div>;
+};
+
+export default Footer;
---
+++ components/shared/Header.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <div>Header Test c:</div>;
+};
+
+export default Header;
