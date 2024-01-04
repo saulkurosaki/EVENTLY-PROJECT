c0e05d67cf0397c9dc103472be44d8f6d5957367
Saul Paredes <saulkurosaki13@gmail.com>
2024-01-04 12:19:07 -0600

    feat(event details page): implement event description section

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -78,13 +78,13 @@ const EventDetails = async ({ params: { id } }: SearchParamProps) => {
           </div>
 
           {/* EVENT DESCRIPTION */}
-          {/* <div className="flex flex-col gap-2">
+          <div className="flex flex-col gap-2">
             <p className="p-bold-20 text-grey-600">What You&apos;ll Learn:</p>
             <p className="p-medium-16 lg:p-regular-18">{event.description}</p>
             <p className="p-medium-16 lg:p-regular-18 truncate text-primary-500 underline">
               {event.url}
             </p>
-          </div> */}
+          </div>
         </div>
       </div>
     </section>
