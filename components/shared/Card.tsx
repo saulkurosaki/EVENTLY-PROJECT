5d04c29624778c0fdc90bdcb3d38c97eb1b2021d
Saul Paredes <saulkurosaki13@gmail.com>
2024-01-04 18:10:18 -0600

    feat(Card): implement complete card display

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -1,4 +1,6 @@
 import { IEvent } from "@/lib/database/models/event.model";
+import { formatDateTime } from "@/lib/utils";
+import Image from "next/image";
 import Link from "next/link";
 
 type CardProps = {
@@ -16,6 +18,47 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
         className="flex-center flex-grow bg-gray-50 bg-cover bg-center text-grey-500"
       />
       {/* IS EVENT CREATOR */}
+      <Link
+        href={`/events/${event._id}`}
+        className="flex min-h-[230px] flex-col gap-3 p-5 md:gap-4"
+      >
+        {!hidePrice && (
+          <div className="flex gap-2">
+            <span className="p-semibold-14 w-min rounded-full bg-green-100 px-4 py-1 text-green-60">
+              {event.isFree ? "FREE" : `$${event.price}`}
+            </span>
+            <p className="p-semibold-14 w-min rounded-full bg-grey-500/10 px-4 py-1 text-grey-500">
+              {event.category.name}
+            </p>
+          </div>
+        )}
+
+        <p className="p-medium-16 p-medium-18 text-grey-500">
+          {formatDateTime(event.startDateTime).dateTime}
+        </p>
+
+        <p className="p-medium-16 md:p-medium-20 line-clamp-2 flex-1 text-black">
+          {event.title}
+        </p>
+
+        <div className="flex-between w-full">
+          <p className="p-medium-14 md:p-medium-16 text-grey-600">
+            {event.organizer.firstName} {event.organizer.lastName}
+          </p>
+
+          {hasOrderLink && (
+            <Link href={`/orders?eventId=${event._id}`} className="flex gap-2">
+              <p className="text-primary-500">Order Details</p>
+              <Image
+                src="/assets/icons/arrow.svg"
+                alt="search"
+                width={10}
+                height={10}
+              />
+            </Link>
+          )}
+        </div>
+      </Link>
     </div>
   );
 };
