ac1af7817b2f79c87a671de5df1db49e0083cde1
Saul Paredes <saulkurosaki13@gmail.com>
2024-01-05 01:06:13 -0600

    feat(event details page): implement checkout button

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,3 +1,4 @@
+import CheckoutButton from "@/components/shared/CheckoutButton";
 import Collection from "@/components/shared/Collection";
 import {
   getEventById,
@@ -59,6 +60,7 @@ const EventDetails = async ({
             </div>
 
             {/* CHECKOUT BUTTON */}
+            <CheckoutButton event={event} />
 
             {/* EVENT INFO */}
             <div className="flex flex-col gap-5">
