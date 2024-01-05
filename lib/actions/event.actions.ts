ae7411af9968407acea30b4950ab5035b5ae1e47
Saul Paredes <saulkurosaki13@gmail.com>
2024-01-04 19:05:37 -0600

    feat(event.actions.ts): implement getEventsByUserId server action

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -144,33 +144,33 @@ export async function deleteEvent({ eventId, path }: DeleteEventParams) {
 // }
 
 // GET EVENTS BY ORGANIZER
-// export async function getEventsByUser({
-//   userId,
-//   limit = 6,
-//   page,
-// }: GetEventsByUserParams) {
-//   try {
-//     await connectToDatabase();
+export async function getEventsByUser({
+  userId,
+  limit = 6,
+  page,
+}: GetEventsByUserParams) {
+  try {
+    await connectToDatabase();
 
-//     const conditions = { organizer: userId };
-//     const skipAmount = (page - 1) * limit;
+    const conditions = { organizer: userId };
+    const skipAmount = (page - 1) * limit;
 
-//     const eventsQuery = Event.find(conditions)
-//       .sort({ createdAt: "desc" })
-//       .skip(skipAmount)
-//       .limit(limit);
+    const eventsQuery = Event.find(conditions)
+      .sort({ createdAt: "desc" })
+      .skip(skipAmount)
+      .limit(limit);
 
-//     const events = await populateEvent(eventsQuery);
-//     const eventsCount = await Event.countDocuments(conditions);
+    const events = await populateEvent(eventsQuery);
+    const eventsCount = await Event.countDocuments(conditions);
 
-//     return {
-//       data: JSON.parse(JSON.stringify(events)),
-//       totalPages: Math.ceil(eventsCount / limit),
-//     };
-//   } catch (error) {
-//     handleError(error);
-//   }
-// }
+    return {
+      data: JSON.parse(JSON.stringify(events)),
+      totalPages: Math.ceil(eventsCount / limit),
+    };
+  } catch (error) {
+    handleError(error);
+  }
+}
 
 // GET RELATED EVENTS: EVENTS WITH SAME CATEGORY
 // export async function getRelatedEventsByCategory({
