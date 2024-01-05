861e122f422617cbe8d6e9ceb1b97d0c728e9639
Saul Paredes <saulkurosaki13@gmail.com>
2024-01-04 19:03:53 -0600

    feat(event.actions.ts): implement getCategoryByName function

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -3,7 +3,7 @@ import { formatDateTime } from "@/lib/utils";
 import { auth } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
-import DeleteConfirmation from "./DeleteConfirmation";
+import { DeleteConfirmation } from "./DeleteConfirmation";
 
 type CardProps = {
   event: IEvent;
