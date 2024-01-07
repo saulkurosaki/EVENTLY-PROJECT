a98cee5640fa67528080550d306fe7de84879390
Saul Paredes <saulkurosaki13@gmail.com>
2024-01-07 12:36:07 -0600

    feat(order.actions.ts): implement getOrdersByUser server action

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -128,41 +128,41 @@ export async function getOrdersByEvent({
 }
 
 // GET ORDERS BY USER
-// export async function getOrdersByUser({
-//   userId,
-//   limit = 3,
-//   page,
-// }: GetOrdersByUserParams) {
-//   try {
-//     await connectToDatabase();
+export async function getOrdersByUser({
+  userId,
+  limit = 3,
+  page,
+}: GetOrdersByUserParams) {
+  try {
+    await connectToDatabase();
 
-//     const skipAmount = (Number(page) - 1) * limit;
-//     const conditions = { buyer: userId };
+    const skipAmount = (Number(page) - 1) * limit;
+    const conditions = { buyer: userId };
 
-//     const orders = await Order.distinct("event._id")
-//       .find(conditions)
-//       .sort({ createdAt: "desc" })
-//       .skip(skipAmount)
-//       .limit(limit)
-//       .populate({
-//         path: "event",
-//         model: Event,
-//         populate: {
-//           path: "organizer",
-//           model: User,
-//           select: "_id firstName lastName",
-//         },
-//       });
+    const orders = await Order.distinct("event._id")
+      .find(conditions)
+      .sort({ createdAt: "desc" })
+      .skip(skipAmount)
+      .limit(limit)
+      .populate({
+        path: "event",
+        model: Event,
+        populate: {
+          path: "organizer",
+          model: User,
+          select: "_id firstName lastName",
+        },
+      });
 
-//     const ordersCount = await Order.distinct("event._id").countDocuments(
-//       conditions
-//     );
+    const ordersCount = await Order.distinct("event._id").countDocuments(
+      conditions
+    );
 
-//     return {
-//       data: JSON.parse(JSON.stringify(orders)),
-//       totalPages: Math.ceil(ordersCount / limit),
-//     };
-//   } catch (error) {
-//     handleError(error);
-//   }
-// }
+    return {
+      data: JSON.parse(JSON.stringify(orders)),
+      totalPages: Math.ceil(ordersCount / limit),
+    };
+  } catch (error) {
+    handleError(error);
+  }
+}
