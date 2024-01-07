6213c520ce663514e9dc4d6542714392ba8e61bd
Saul Paredes <saulkurosaki13@gmail.com>
2024-01-07 14:01:52 -0600

    feat(Collection): implement Collection component initial layout

---
+++ components/shared/Pagination.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { useRouter, useSearchParams } from "next/navigation";
+import { Button } from "../ui/button";
+
+type PaginationProps = {
+  page: number | string;
+  totalPages: number;
+  urlParamName?: string;
+};
+
+const Pagination = ({ page, totalPages, urlParamName }: PaginationProps) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const onClick = (btnType: string) => {
+    // Logic
+  };
+
+  return (
+    <div className="flex gap-2">
+      <Button
+        size="lg"
+        variant="outline"
+        className="w-28"
+        onClick={() => onClick("prev")}
+        disabled={Number(page) <= 1}
+      >
+        Previous
+      </Button>
+
+      <Button
+        size="lg"
+        variant="outline"
+        className="w-28"
+        onClick={() => onClick("next")}
+        disabled={Number(page) >= totalPages}
+      >
+        Next
+      </Button>
+    </div>
+  );
+};
+
+export default Pagination;
