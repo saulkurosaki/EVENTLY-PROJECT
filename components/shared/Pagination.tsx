4f61560a07a894bb322842ee8a28c88702a86ec8
Saul Paredes <saulkurosaki13@gmail.com>
2024-01-07 19:19:46 -0600

    feat(Pagination): implement complete Pagination component

--- components/shared/Pagination.tsx
+++ components/shared/Pagination.tsx
@@ -2,6 +2,7 @@
 
 import { useRouter, useSearchParams } from "next/navigation";
 import { Button } from "../ui/button";
+import { formUrlQuery } from "@/lib/utils";
 
 type PaginationProps = {
   page: number | string;
@@ -14,7 +15,15 @@ const Pagination = ({ page, totalPages, urlParamName }: PaginationProps) => {
   const searchParams = useSearchParams();
 
   const onClick = (btnType: string) => {
-    // Logic
+    const pageValue = btnType === "next" ? Number(page) + 1 : Number(page) - 1;
+
+    const newUrl = formUrlQuery({
+      params: searchParams.toString(),
+      key: urlParamName || "page",
+      value: pageValue.toString(),
+    });
+
+    router.push(newUrl, { scroll: false });
   };
 
   return (
